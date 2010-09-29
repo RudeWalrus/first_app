8452350e582a4f8f0ce2a42983c179f10b4a0732
SerialStream <michael.sobelman@gmail.com>
2010-09-28 18:32:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6ca7655ec80409f6e58a1ee8b5732fd6c25c6fa1c989c7411b09998f3da1796223b2db1238d65a4ba339cbd48a6805504fb54e830aa58a5cbd83f4cce2a0f7be'
